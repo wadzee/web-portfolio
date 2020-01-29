667c4daeb0c7252759172bbd687e5a0386941fd2
Radzi Ramli <wadzee_8@hotmail.com>
2020-01-30 00:12:00 +0800

    refactor code

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ module.exports = {
   theme: {
     color: {
       primary: '#45b29a',
-      secondary: 'rgb(100, 255, 218)',
-      tertiary: 'rgb(204,214,246)',
-      dim: '#bbb',
-      danger: '#e9235a'
+      secondary: 'rgb(100, 255, 218)'
     }
   }
 }
